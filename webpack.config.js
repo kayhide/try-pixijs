d18ba96576f28a99edf169c4ff2125845fefcac5
kayhide <kayhide@gmail.com>
2018-11-25 16:24:05 +0900

    update packages and webpack settings.

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,26 @@
 const webpack = require('webpack');
 const path = require('path');
-
-/*
- * We've enabled Postcss, autoprefixer and precss for you. This allows your app
- * to lint  CSS, support variables and mixins, transpile future CSS syntax,
- * inline images, and more!
- *
- * To enable SASS or LESS, add the respective loaders to module.rules
- *
- * https://github.com/postcss/postcss
- *
- * https://github.com/postcss/autoprefixer
- *
- * https://github.com/jonathantneal/precss
- *
- */
-
 const autoprefixer = require('autoprefixer');
 const precss = require('precss');
 
-/*
- * We've enabled MiniCssExtractPlugin for you. This allows your app to
- * use css modules that will be moved into a separate CSS file instead of inside
- * one of your module entries!
- *
- * https://github.com/webpack-contrib/mini-css-extract-plugin
- *
- */
-
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
 
 module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.js$/,
 				include: [path.resolve(__dirname, 'src')],
-				loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
 
-				options: {
-					plugins: ['syntax-dynamic-import'],
+        use: {
+          loader: 'babel-loader',
 
-					presets: [
-						[
-							'env',
-							{
-								modules: false
-							}
-						]
-					]
-				},
-
-				test: /\.js$/
+          options: {
+            plugins: ["@babel/plugin-syntax-dynamic-import"],
+            presets: ['@babel/preset-env']
+          }
+        }
 			},
 			{
 				test: /\.css$/,
@@ -114,10 +71,12 @@ module.exports = {
 
   plugins: [
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
       filename: "[name].css",
       chunkFilename: "[id].css"
     })
-  ]
+  ],
+
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  }
 };
